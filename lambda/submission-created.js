5556ca99a41bc6a35eb23739be3b5c66e1670d10
Ellen Probst <ellenprobst@Ellens-MBP.hitronhub.home>
2022-10-18 19:45:19 -0400

    log

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -29,35 +29,39 @@ exports.handler = async (event) => {
   const { payload } = JSON.parse(event.body)
   const { naam, email, vraag, images } = payload.data
   const formName = payload.data['form-name']
-  console.log(payload, JSON.parse(images))
-  if (formName === FORM.vraag) {
-    const { data, errors } = await sendQuery(CREATE_ITEM, {
-      naam,
-      datum: new Date(Date.now()),
-      email,
-      vraag,
-      images: JSON.parse(images),
-    })
+  console.log(payload, images)
+  // if (formName === FORM.vraag) {
+  //   const { data, errors } = await sendQuery(CREATE_ITEM, {
+  //     naam,
+  //     datum: new Date(Date.now()),
+  //     email,
+  //     vraag,
+  //     images: JSON.parse(images),
+  //   })
 
-    if (errors) {
-      console.log(errors)
-      return {
-        statusCode: 500,
-        body: JSON.stringify(errors),
-      }
-    }
-    console.log('done')
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        nieuweVraag: data.createItem,
-      }),
-    }
-  }
+  //   if (errors) {
+  //     console.log(errors)
+  //     return {
+  //       statusCode: 500,
+  //       body: JSON.stringify(errors),
+  //     }
+  //   }
+  //   console.log('done')
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       nieuweVraag: data.createItem,
+  //     }),
+  //   }
+  // }
 
   if (formName === FORM.contact) {
     return {
       statusCode: 200,
     }
+  }
+
+  return {
+    statusCode: 200,
   }
 }
