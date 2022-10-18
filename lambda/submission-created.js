55128dbdd0d68b082cbbdb139f55b81bef6cdf77
Ellen Probst <ellenprobst@Ellens-MBP.hitronhub.home>
2022-10-18 19:02:38 -0400

    debug logs

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -1,4 +1,8 @@
 const sendQuery = require('./utils/send-query')
+const FORM = {
+  vraag: 'vragen-formulier',
+  contact: 'contact',
+}
 
 const CREATE_ITEM = `mutation($naam: String!,$datum: Time!,$email: String!,$vraag: String!, $images: [ImageInput]) {
   createVraag(data:{
@@ -24,26 +28,36 @@ const CREATE_ITEM = `mutation($naam: String!,$datum: Time!,$email: String!,$vraa
 exports.handler = async (event) => {
   const { payload } = JSON.parse(event.body)
   const { naam, email, vraag, images } = payload.data
+  const formName = payload.data['form-name']
+  console.log(payload, JSON.parse(images))
+  if (formName === FORM.vraag) {
+    const { data, errors } = await sendQuery(CREATE_ITEM, {
+      naam,
+      datum: new Date(Date.now()),
+      email,
+      vraag,
+      images: JSON.parse(images),
+    })
 
-  const { data, errors } = await sendQuery(CREATE_ITEM, {
-    naam,
-    datum: new Date(Date.now()),
-    email,
-    vraag,
-    images: JSON.parse(images),
-  })
-
-  if (errors) {
+    if (errors) {
+      console.log(errors)
+      return {
+        statusCode: 500,
+        body: JSON.stringify(errors),
+      }
+    }
+    console.log('done')
     return {
-      statusCode: 500,
-      body: JSON.stringify(errors),
+      statusCode: 200,
+      body: JSON.stringify({
+        nieuweVraag: data.createItem,
+      }),
     }
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      nieuweVraag: data.createItem,
-    }),
+  if (formName === FORM.contact) {
+    return {
+      statusCode: 200,
+    }
   }
 }
